16bd8ed7bf4a74049aae850331ae5a2edbebff23
Corwin96 <33621431+Corwin96@users.noreply.github.com>
2019-11-24 12:27:48 +0100

    Create itemfactory.sol
    For adding new items to the game, the createItem function requires some work to make sure every item name is linked to an id and different items can't use the same id.

---
+++ Day_02_Wed_20_11_2019/itemfactory.sol
@@ -0,0 +1,29 @@
+pragma solidity >=0.5.0 <0.7.0;
+
+import "./characterfactory.sol";
+
+/**
+ * @author Corwin van Dalen
+ * @title add items to the game.
+ */
+contract ItemFactory{
+    event NewItem(uint itemId, string itemType, uint itemStats);
+    
+    struct Item {
+        uint itemId;
+        string itemType;
+        uint itemStats;
+    }
+    
+    Item[] public items;
+    
+    mapping (string => uint) typeToId;
+    
+    //Create a new item, every unique item can only be added a single time.
+    function createItem(uint _itemId, string memory _itemType, uint _itemStats) internal {
+        require(_itemId != typeToId[_itemType]);
+        items.push(Item(_itemId, _itemType, _itemStats));
+        typeToId[_itemType] = _itemId;
+        emit NewItem(_itemId, _itemType, _itemStats);
+    }
+}
