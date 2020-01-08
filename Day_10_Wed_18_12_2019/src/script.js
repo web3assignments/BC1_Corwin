e43d472518a7f5b95f1e1e6dbd831eca90e2552b
Corwin96 <corwinvandalen@gmail.com>
2020-01-08 15:53:53 +0100

    Working file upload

--- Day_10_Wed_18_12_2019/src/script.js
+++ Day_10_Wed_18_12_2019/src/script.js
@@ -2,21 +2,39 @@ function log(logstr) {
     document.getElementById("log").innerHTML +=logstr+"\n";
 }
 
+const ipfs = window.IpfsHttpClient('https://ipfs.infura.io:5001');
+
 async function addFile() {
-    var str=document.getElementById("stringtoadd").value; // note: trailing space, CR, LF
-    const Buffer=window.buffer.Buffer;
-    const ipfs = window.IpfsHttpClient('https://ipfs.infura.io:5001');         
-    const bufDataToHash = Buffer.from(str);
-    log(`Adding ${str} to ipfs via Infura`);
-    const hash=await ipfs.add(bufDataToHash).catch(console.log); 
-    log(`Hash ${hash[0].hash}`);
+    let fileFormats = [];
+    let ipfsId;
+    const files = document.querySelector('[type=file]').files;
+        
+        for (let i = 0; i < files.length; i++) {
+            let file = files[i];
+            const fileformat = {
+                path: file.name,
+                content: file
+            };
+            fileFormats[i] = fileformat;
+        }
+
+        console.log(fileFormats);
+
+        const options = {
+            wrapWithDirectory: true,
+            progress: (prog) => console.log(`received: ${prog}`)
+        };
+
+        ipfs.add(fileFormats, options).then((response) => {
+            console.log(response);
+            ipfsId = response[response.length - 1].hash;
+            log(`The hash for the file is: ${ipfsId}`);
+        }).catch((err) => {
+            console.error(err);
+        });    
 }
 
 async function retrieveFile() {
-    var hash= document.getElementById("hashtoretrieve").value;
-    const ipfs = window.IpfsHttpClient('https://ipfs.infura.io:5001');         
-    log(`Checking hash ${hash} via Infura`)
-    const file = await ipfs.cat(hash).catch(x=> log(`Error ${x}`));
-    if (file)
-        log(file.toString('utf8'));
+    var hash= document.getElementById("hashtoretrieve").value;         
+    log(`ipfs.io/ipfs/${hash}`)  
 }
